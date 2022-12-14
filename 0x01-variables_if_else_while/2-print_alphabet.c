263c65d175780210d17dadb40ebbfb42cf4e773b
Prosper Somba Yabido <prospersombayabido@gmail.com>
2022-12-13 17:24:23 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	 putchar('\n');
+	putchar('\n');
 
-	 return (0);
+	return (0);
 }
