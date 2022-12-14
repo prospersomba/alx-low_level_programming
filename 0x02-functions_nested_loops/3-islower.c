a6701545675244b605a019f4d4d0548fc74b1947
Prosper Somba Yabido <prospersombayabido@gmail.com>
2022-12-13 17:17:35 -0800

    fibonacci

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _islower - a function that checks for lowercase character
- * @c: single letter input
- * Return: 1 if int c is lowercase, 0 if otherwise
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
